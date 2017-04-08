ab4345896cd4e4f07ceeb208be8e047d71a6c69c
AnnGabr <annabaks1997@gmail.com>
2017-04-09 01:08:26 +0300

    added files with tables constants

---
+++ dbManager/tablesInfo/ComTestMode.java
@@ -0,0 +1,19 @@
+package dbManager.tablesInfo;
+
+/**
+ * Created by Ann on 23.03.2017.
+ */
+
+public final class ComTestMode {
+    public static final String CARTOONS_TABLE_NAME = "CARTOONS";
+    public static final String SERIES_TABLE_NAME = "SERIES";
+    public static final String FILMS_TABLE_NAME = "FILMS";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_QUESTION = "question";
+    public static final String COLUMN_IMG = "image";
+    public static final String COLUMN_RIGHT_ANSWER = "right";
+    public static final String COLUMN_A = "a";
+    public static final String COLUMN_B = "b";
+    public static final String COLUMN_C = "c";
+}
